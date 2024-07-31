c2f8751fb46f6228531206a1b939526977941b4e
kieransweetman <kikisweetman@gmail.com>
2024-07-31 17:51:09 +0200

    dev env tinkering for not having to precompile assets. not a real solution, need to fix/replace
    helper function that should be split but is here for now
    
    ignoring asset/images for now

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# all melee characters records
 class Character < ApplicationRecord
+  def image_file_name
+    subfolder = name.downcase.gsub(' ', '_')
+    files = Dir.glob(Rails.root.join('app', 'assets', 'images', 'portraits', subfolder, '*'))
+    files.any? ? "/assets/portraits/#{subfolder}/#{File.basename(files.first)}" : nil
+  end
 end
